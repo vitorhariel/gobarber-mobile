ab932b60d0056eb0cea71948693e07d376cf2457
Vitor Hariel <vhbarauna@gmail.com>
2019-08-09 13:24:51 -0300

    Input and Button components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Text>Sign In</Text>;
+  return (
+    <Background>
+      <Text>Sign In</Text>
+
+      <Input icon="call" placeholder="Digite seu nome" />
+      <Button>Entrar</Button>
+    </Background>
+  );
 }
